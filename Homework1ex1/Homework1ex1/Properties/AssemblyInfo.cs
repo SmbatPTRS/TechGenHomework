76194f88f21b337951826028f0bff61d4eff0ca0
Smbat Petrosyan <I21.Petrosyan.Smbat@etud.ufar.am>
2026-04-29 16:15:10 +0400

    hw 2, hw 3

--- Homework1ex1/Homework1ex1/Properties/AssemblyInfo.cs
+++ Homework1ex1/Homework1ex1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HomeWorkTechGen")]
+[assembly: AssemblyTitle("Homework1ex1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HomeWorkTechGen")]
+[assembly: AssemblyProduct("Homework1ex1")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("acc1c0a7-ad21-4162-86ae-85e1372a315d")]
+[assembly: Guid("b0d90e5c-0527-432b-91e4-087da121d2fe")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Homewrok1ex3/Homewrok1ex3/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1ex3
+{
+    internal class MyList
+    {
+        private int[] _items;
+        private int count;
+
+        public MyList()
+        {
+            _items = new int[4];
+            count = 0;
+        }
+
+        public void Add(int item)
+        {
+            if (count == _items.Length)
+            {
+                Resize();
+            }
+
+            _items[count] = item;
+            count++;
+        }
+
+        public void AddRange(int[] items)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Add(items[i]);
+            }
+        }
+
+        public bool Remove(int item)
+        {
+            int index = -1;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (_items[i] == item)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            for (int i = index; i < count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+
+            count--;
+
+            return true;
+        }
+
+        public bool TryGet(int index, out int value)
+        {
+            if (index < 0 || index >= count)
+            {
+                value = 0;
+                return false;
+            }
+
+            value = _items[index];
+            return true;
+        }
+
+        private void Resize()
+        {
+            int[] new_array = new int[_items.Length * 2];
+
+            for (int i = 0; i < _items.Length; i++)
+            {
+                new_array[i] = _items[i];
+            }
+
+            _items = new_array;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(_items[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list = new MyList();
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            list.Print();
+
+            int[] arr = { 40, 50, 60 };
+
+            list.AddRange(arr);
+
+            list.Print();
+
+            list.Remove(20);
+
+            list.Print();
+
+            int value;
+
+            bool found = list.TryGet(2, out value);
+
+            Console.WriteLine(found);
+
+            if (found)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
