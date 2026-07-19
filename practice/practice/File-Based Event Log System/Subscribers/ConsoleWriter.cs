8ae6abea5a9cdc336b357cad21d9da37dae7f5cc
Smbat Petrosyan <I21.Petrosyan.Smbat@etud.ufar.am>
2026-07-19 18:46:04 +0400

    implementing several tools in Log system

---
+++ practice/practice/File-Based Event Log System/Subscribers/ConsoleWriter.cs
@@ -0,0 +1,10 @@
+﻿namespace practice.File_Based_Event_Log_System.Subscribers;
+
+public class ConsoleWriter
+{
+    public void LogConsole(object sender, LogEventArgs logEventArgs)
+    {
+        Console.WriteLine($"{logEventArgs.Message},{logEventArgs.level},{logEventArgs.TimeStamp}");
+        Console.WriteLine();
+    }
+}
---
+++ practice/practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using practice.File_Based_Event_Log_System.Subscribers;
+
+namespace practice;
+using practice.File_Based_Event_Log_System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string filepath = Path.Combine(Environment.CurrentDirectory, "log.txt");
+       Logger log = new Logger();
+       ConsoleWriter consoleWriter = new ConsoleWriter();
+       LogFileWriter logFileWriter = new LogFileWriter(filepath);
+       log.LogRaised += consoleWriter.LogConsole;
+       log.LogRaised += logFileWriter.WriteLog;
+       
+       log.Log("System started", LogLevel.Info);
+       log.Log("Low disk space warning", LogLevel.Warning);
+       log.Log("Database connection dropped!", LogLevel.Error);
+       
+       Console.WriteLine("\n--- Printing History from the Logger Storage ---");
+       
+       foreach (LogEventArgs entry in log)
+       {
+           Console.WriteLine($"History: [{entry.TimeStamp}] {entry.Message}");
+       }
+    }
+    
+}
+
+
+
