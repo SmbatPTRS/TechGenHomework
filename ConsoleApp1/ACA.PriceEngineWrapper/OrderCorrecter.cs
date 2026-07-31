6f440c2f7a6352b6d64854a62d9128ef1a8a7035
Smbat Petrosyan <I21.Petrosyan.Smbat@etud.ufar.am>
2026-07-31 12:48:01 +0400

    price engine wrapper

---
+++ ConsoleApp1/ACA.PriceEngineWrapper/OrderCorrecter.cs
@@ -0,0 +1,70 @@
+﻿namespace ACA.PriceEngineWrapper;
+using ACA.PriceEngine;
+using System;
+using System.Reflection;
+public class OrderCorrecter
+{
+    Assembly _assembly = Assembly.Load("ACA.PriceEngine");
+
+
+    public object Run(PriceInput input)
+    {
+        Type type = _assembly.GetType("ACA.PriceEngine.PriceEngine");
+
+        
+        if (type == null)
+        {
+            Console.WriteLine("Couldn't find PriceEngine");
+            return null;
+        }
+        
+        object obj = Activator.CreateInstance(type);
+
+        if (obj == null)
+        {
+            Console.WriteLine("Couldn't create instance of PriceEngine");
+            return null;
+        }
+        
+         
+        MethodInfo? computeSubtotal     = type.GetMethod("ComputeSubtotal", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? countUnits          = type.GetMethod("CountUnits", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? applyVolumeDiscount = type.GetMethod("ApplyVolumeDiscount", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? applyLoyaltyDiscount= type.GetMethod("ApplyLoyaltyDiscount", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? applyCoupon         = type.GetMethod("ApplyCoupon", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? applyVat            = type.GetMethod("ApplyVat", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? roundMoney          = type.GetMethod("RoundMoney", BindingFlags.NonPublic | BindingFlags.Static);
+
+        if (computeSubtotal == null || countUnits == null || applyVolumeDiscount == null ||
+            applyLoyaltyDiscount == null || applyCoupon == null || applyVat == null || roundMoney == null)
+        {
+            Console.WriteLine("Failed to load one or more private methods.");
+            return null;
+        }
+        MethodInfo? method1 = type.GetMethod("ComputeSubtotal", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? method2 = type.GetMethod("CountUnits", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? method3 = type.GetMethod("ApplyVolumeDiscount", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? method4 = type.GetMethod("ApplyLoyaltyDiscount", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? method5 = type.GetMethod("ApplyCoupon", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? method6 = type.GetMethod("ApplyVat", BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo? method7 = type.GetMethod("RoundMoney", BindingFlags.NonPublic | BindingFlags.Static); // Note: Static!
+
+        // --- Execute Calculations Step-by-Step ---
+
+        object subtotal = method1.Invoke(obj, new object[] { input.Lines });
+
+        object totalUnits = method2.Invoke(obj, new object[] { input.Lines });
+
+        object afterVolume = method3.Invoke(obj, new object[] { subtotal, totalUnits });
+
+        object afterLoyalty = method4.Invoke(obj, new object[] { afterVolume, input.LoyaltyTier });
+
+        object afterCoupon = method5.Invoke(obj, new object[] { afterLoyalty, input.CouponAmount });
+
+        object afterVat = method6.Invoke(obj, new object[] { afterCoupon, input.VatRate });
+
+        object finalResult = method7.Invoke(null, new object[] { afterVat });
+
+        return finalResult;
+    }
+}
